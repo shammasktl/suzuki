c51d4989b0d082162f903c7e41ddafab59a5f1c0
shammasktl <mshammast370737@gmail.com>
2024-11-30 16:07:55 +0530

    Added Content

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./Content.css"
 import { assets } from '../../assets/assets'
+import { IoArrowForward } from 'react-icons/io5'
 
 const Content = () => {
   return (
     <div className='content'>
       <h2>Ready to <span>Race</span></h2>
-      <a href="#" className='btn'>Ride Now</a>
+      <a href="#" className='btn'>Ride Now <span><IoArrowForward /></span></a>
       <img src={assets.bike} className='bike' alt="" />
     </div>
   )
--- src/Components/Slider Content/SliderContent.jsx
+++ src/Components/Slider Content/SliderContent.jsx
@@ -4,9 +4,17 @@ import "./SliderContent.css"
 const SliderContent = () => {
     return (
         <div className='slider'>
-            <div className='slides'>
+            <div className='slides active'>
                 <h2><span>Engine</span><br />599 cc</h2>
                 <h2><span>Max Speed</span><br />240 Km/h</h2>
+            </div>
+            <div className='slides'>
+                <h2><span>Mileage</span><br />14 Km/L</h2>
+                <h2><span>0 - 100 Km/H</span><br />4 Seconds</h2>
+            </div>
+            <div className='slides'>
+                <h2><span>Wheels</span><br />17-Inch Alloy</h2>
+                <h2><span>Tyre Type</span><br />260 Km/H</h2>
             </div>
         </div>
     )
